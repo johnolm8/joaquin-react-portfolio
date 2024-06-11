68006fac509e95f4221a49f2dfdee8e195faaff5
johnolm8 <144188427+johnolm8@users.noreply.github.com>
2024-06-11 18:39:28 +0000

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'bulma/css/bulma.css';
+import './css/animate.css';
+import './css/App.css'
+import App from './App'
+
+render(<App />, document.getElementById('root'));
